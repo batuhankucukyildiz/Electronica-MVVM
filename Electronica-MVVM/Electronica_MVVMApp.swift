f3b14bc299192a1b0b2baeeb969a958b6a58735d
Batuhan <32312869+batuhankucukyildiz@users.noreply.github.com>
2023-08-17 08:29:17 +0300

    Initial Commit

---
+++ Electronica-MVVM/Electronica_MVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  Electronica_MVVMApp.swift
+//  Electronica-MVVM
+//
+//  Created by Batuhan Kucukyildiz on 17.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct Electronica_MVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
